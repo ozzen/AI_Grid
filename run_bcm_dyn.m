f570df644365c8ee947cec8948f9e278cfd496fe
ozzen <shroy@cs.stonybrook.edu>
2022-07-10 15:18:09 -0400

    Update run_bcm_dyn.m

--- run_bcm_dyn.m
+++ run_bcm_dyn.m
@@ -8,15 +8,15 @@ Vd2 = 0.485249524;
 Vq2 = -0.018170484;
 VBat2ds = 0.246385332;
 VBat2qs = 1.1901986;
-% Ild2 = Id2;
-% Ilq2 = Iq2;
+
+dt = 0.0001;
 
 % ODEsolver
 for i = 1:600000
-    t = 0:0.0001:0.0002;
-%     tic;
+    t = 0:dt:2*dt;
+    tic;
     [t,x] = ode45(@(t,x)der_dyn(t,x),t,[Id2;Iq2;Vd2;Vq2;VBat2ds;VBat2qs]);
-%     toc;
+    toc;
     s(i,:) = x(2,:);
     Id2 = x(2,1);
     Iq2 = x(2,2);
@@ -24,12 +24,10 @@ for i = 1:600000
     Vq2 = x(2,4);
     VBat2ds = x(2,5);
     VBat2qs = x(2,6);
-%     Ild2 = x(2,7);
-%     Ilq2 = x(2,8);
 end
 
-plot(x(:,3))
-% axis([0 10 0.48 0.49])
+X = (linspace(0,i*dt,length(s))).';
+plot(X,s(:,3))
 
 % Dynamic Equations
 function f = der_dyn(t,z)
@@ -45,14 +43,8 @@ Vd2 = z(3);
 Vq2 = z(4);
 VBat2ds = z(5);
 VBat2qs = z(6);
-% Ild2 = z(7);
-% Ilq2 = z(8);
 
 w = 1/(2*pi*60);
-I_mg_d = 0.16;
-I_mg_q = 0.09;
-V_mg_d = 12.47;
-V_mg_q = 0.66;
 Id2_ref = -0.618;
 Iq2_ref = 0.0;
 
@@ -64,7 +56,5 @@ f(3) = w*Vd2 - (Id2_ref - Id2)/C;
 f(4) = -w*Vq2 - (Iq2_ref - Iq2)/C;
 f(5) = Vd2 + w*L*Iq2 - K*(Id2_ref - Id2);
 f(6) = Vq2 - w*L*Id2 - K*(Iq2_ref - Iq2);
-% f(7) = -R*Id2 + w*Iq2 + Vd2 - V_mg_d;
-% f(8) = -R*Iq2 - w*Id2 + Vq2 - V_mg_q;
 
 end
